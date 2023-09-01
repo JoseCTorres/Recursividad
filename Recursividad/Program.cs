86368cb45a46534fe7be4731dd625980cd72c28c
torre <torre@LAPTOP-4GQN8FL3>
2023-09-01 13:09:45 -0600

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("igresar el numero para calular el factorial");
+            int numero= Convert.ToInt32(Console.ReadLine());
+
+            int numerof = factorial(numero);
+
+            Console.WriteLine("El factorial de " + numero + "es igual a " + numerof);
+        }
+          public static int factorial  (int numero)
+        {
+        if (numero == 1) 
+            { 
+                return 1;
+            }
+        else
+            {
+                return numero * factorial(numero-1);
+            }
+        }
+    }
+}
